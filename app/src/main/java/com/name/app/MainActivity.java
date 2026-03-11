7a8f327788bfcb5888bee0d50b2bc3373af44e1a
f3be20b4-2c39-4c6a-b6f5-76ded0d8e655 <erandix9@gmail.com>
2026-03-11 11:03:56 +0300

    Updatted

--- app/src/main/java/com/name/app/MainActivity.java
+++ app/src/main/java/com/name/app/MainActivity.java
@@ -9,6 +9,7 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -18,23 +19,32 @@ public class MainActivity extends AppCompatActivity {
     private WebView webView;
     private final int PERMISSION_REQUEST_CODE = 100;
 
+    // Singleton holder to reference MainActivity from SmsReceiver
+    private static MainActivity instance;
+    public static MainActivity getInstance() { return instance; }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = this;
 
+        // Initialize WebView
         webView = new WebView(this);
         setContentView(webView);
 
-        // Setup WebView
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html"); // local HTML file
 
-        // Request permissions
+        // Request permissions if not granted
         if (!hasPermissions()) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS, Manifest.permission.CALL_PHONE},
+                    new String[]{
+                            Manifest.permission.READ_SMS,
+                            Manifest.permission.RECEIVE_SMS,
+                            Manifest.permission.CALL_PHONE
+                    },
                     PERMISSION_REQUEST_CODE);
         } else {
             initUSSD();
@@ -47,33 +57,43 @@ public class MainActivity extends AppCompatActivity {
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED;
     }
 
+    // Initialize USSD request
     private void initUSSD() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
-            tm.sendUssdRequest("*123#", new TelephonyManager.UssdResponseCallback() {
-                @Override
-                public void onReceiveUssdResponse(@NonNull TelephonyManager telephonyManager, @NonNull String request, @NonNull CharSequence response) {
-                    // Send USSD response to WebView
-                    runOnUiThread(() -> webView.evaluateJavascript("displayUSSD('" + response.toString() + "')", null));
-                }
+            try {
+                TelephonyManager tm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+                tm.sendUssdRequest("*123#", new TelephonyManager.UssdResponseCallback() {
+                    @Override
+                    public void onReceiveUssdResponse(@NonNull TelephonyManager telephonyManager, @NonNull String request, @NonNull CharSequence response) {
+                        // Send USSD response to WebView
+                        runOnUiThread(() -> webView.evaluateJavascript("displayUSSD('" + escapeJS(response.toString()) + "')", null));
+                    }
 
-                @Override
-                public void onReceiveUssdResponseFailed(@NonNull TelephonyManager telephonyManager, @NonNull String request, int failureCode) {
-                    runOnUiThread(() -> Toast.makeText(MainActivity.this, "USSD Failed", Toast.LENGTH_SHORT).show());
-                }
-            }, null);
+                    @Override
+                    public void onReceiveUssdResponseFailed(@NonNull TelephonyManager telephonyManager, @NonNull String request, int failureCode) {
+                        runOnUiThread(() -> Toast.makeText(MainActivity.this, "USSD Failed", Toast.LENGTH_SHORT).show());
+                    }
+                }, null);
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "USSD Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 
-    // Permission result
+    // Escape quotes for JS injection
+    private String escapeJS(String input) {
+        return input.replace("'", "\\'").replace("\n", "\\n").replace("\r", "");
+    }
+
+    // Permission result callback
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_REQUEST_CODE) {
             boolean granted = true;
-            for (int res : grantResults) {
-                if (res != PackageManager.PERMISSION_GRANTED) granted = false;
-            }
+            for (int res : grantResults) if (res != PackageManager.PERMISSION_GRANTED) granted = false;
+
             if (granted) {
                 initUSSD();
             } else {
@@ -82,8 +102,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // Called from SmsReceiver to display SMS in WebView
+    // Method called from SmsReceiver to send SMS content to WebView
     public void displaySMS(String msg) {
-        runOnUiThread(() -> webView.evaluateJavascript("displaySMS('" + msg + "')", null));
+        runOnUiThread(() -> webView.evaluateJavascript("displaySMS('" + escapeJS(msg) + "')", null));
+    }
+
+    // Optional: clean up WebView on destroy
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (webView != null) {
+            webView.loadUrl("about:blank");
+            webView.destroy();
+        }
     }
 }
--- app/src/main/java/com/name/app/SmsReceiver.java
+++ app/src/main/java/com/name/app/SmsReceiver.java
@@ -10,24 +10,26 @@ public class SmsReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Bundle bundle = intent.getExtras();
-        if (bundle != null) {
-            Object[] pdus = (Object[]) bundle.get("pdus");
-            if (pdus != null) {
-                StringBuilder smsBody = new StringBuilder();
-                for (Object pdu : pdus) {
-                    SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdu);
-                    smsBody.append(sms.getMessageBody());
-                }
+        try {
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                if (pdus != null && pdus.length > 0) {
+                    StringBuilder smsBody = new StringBuilder();
+                    for (Object pdu : pdus) {
+                        SmsMessage sms = SmsMessage.createFromPdu((byte[]) pdu);
+                        smsBody.append(sms.getMessageBody());
+                    }
 
-                // Send SMS to MainActivity WebView
-                Intent i = new Intent(context, MainActivity.class);
-                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                MainActivity mainActivity = MainActivityHolder.getInstance();
-                if (mainActivity != null) {
-                    mainActivity.displaySMS(smsBody.toString());
+                    // Send SMS content to WebView if MainActivity is active
+                    MainActivity mainActivity = MainActivity.getInstance();
+                    if (mainActivity != null) {
+                        mainActivity.displaySMS(smsBody.toString());
+                    }
                 }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
